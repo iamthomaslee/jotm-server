0de20f5213b115382d5235d5c32c30dde44e1f8b
Thomas B. Lee <leez79@gmail.com>
2018-10-14 13:31:13 -0700

    allow access root

--- server.js
+++ server.js
@@ -23,6 +23,12 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 // Put all API endpoints under '/api'
 app.get('/api/ping', (req, res) => {
     res.send('hello world')
